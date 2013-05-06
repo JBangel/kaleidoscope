48a9b2c44d56bdc66478d1bbf7cf9c10279aadf6
Joshua Bangel <falornan@gmail.com>
2013-05-06 11:56:03 -0500

    Moved main into object

--- Project Euler/scala/problem1.scala
+++ Project Euler/scala/problem1.scala
@@ -1,6 +1,8 @@
 #!/usr/bin/env scala
 
 object Problem1 {
+  def main(args:Array[String]) = println(sum_multiples(List(3,5), 1 to 999))
+
   def sum_multiples(array:List[Int], range:Range) = {
     range.fold(0) { (total, num) =>
       if (array.exists(x => num % x == 0))
@@ -9,7 +11,4 @@ object Problem1 {
         total
     }
   }
-
-  def main(args:Array[String]) = println(sum_multiples(List(3,5), 1 to 999))
 }
-

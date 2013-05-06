3b867e0e9fb6446d2ca49c59f629e2e6e7c1668f
Joshua Bangel <falornan@gmail.com>
2013-05-06 00:29:05 -0500

    Switched if statement to match

--- FizzBuzz/scala/fizzbuzz.scala
+++ FizzBuzz/scala/fizzbuzz.scala
@@ -1,10 +1,10 @@
 #!/usr/bin/env scala
 
-def fbprint(x: Int) = {
-  if      (x % 15 == 0) "FizzBuzz"
-  else if (x % 3 == 0)  "Fizz"
-  else if (x % 5 == 0)  "Buzz"
-  else                  x
+def fbprint(x:Int) = x match {
+  case i:Int if i % 15 == 0 => "FizzBuzz"
+  case i:Int if i %  3 == 0 => "Fizz"
+  case i:Int if i %  5 == 0 => "Buzz"
+  case _                    => x
 }
 
 (1 until 100).foreach { x =>

2b88fbae8647d9744275698aa82d0a4c2d53871e
Joshua Bangel <falornan@gmail.com>
2013-05-06 01:08:38 -0500

    Project Euler - Scala - Problem 1

---
+++ Project Euler/scala/problem1.scala
@@ -0,0 +1,14 @@
+#!/usr/bin/env scala
+
+object Problem1 {
+  def sum_multiples(array:List[Int], range:Range) = {
+    range.fold(0) { (total, num) =>
+      if (array.exists(x => num % x == 0))
+        (total + num)
+      else
+        total
+    }
+  }
+}
+
+println(Problem1.sum_multiples(List(3,5), 1 to 999))
